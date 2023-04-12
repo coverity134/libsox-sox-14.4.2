c80d084ce415b3b8295bec60baf2782631e4f576
coverity134 <129744590+coverity134@users.noreply.github.com>
2023-04-12 19:44:29 +0800

    Add files via upload

---
+++ libgsm/CMakeFiles/gsm.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/raoxue/Documents/cbmc_verifed_juliet_test_case_ground_truth_new/libsox-sox-14.4.2/libgsm/add.c" "/home/raoxue/Documents/cbmc_verifed_juliet_test_case_ground_truth_new/libsox-sox-14.4.2/libgsm/CMakeFiles/gsm.dir/add.o"
+  "/home/raoxue/Documents/cbmc_verifed_juliet_test_case_ground_truth_new/libsox-sox-14.4.2/libgsm/code.c" "/home/raoxue/Documents/cbmc_verifed_juliet_test_case_ground_truth_new/libsox-sox-14.4.2/libgsm/CMakeFiles/gsm.dir/code.o"
+  "/home/raoxue/Documents/cbmc_verifed_juliet_test_case_ground_truth_new/libsox-sox-14.4.2/libgsm/decode.c" "/home/raoxue/Documents/cbmc_verifed_juliet_test_case_ground_truth_new/libsox-sox-14.4.2/libgsm/CMakeFiles/gsm.dir/decode.o"
+  "/home/raoxue/Documents/cbmc_verifed_juliet_test_case_ground_truth_new/libsox-sox-14.4.2/libgsm/gsm_create.c" "/home/raoxue/Documents/cbmc_verifed_juliet_test_case_ground_truth_new/libsox-sox-14.4.2/libgsm/CMakeFiles/gsm.dir/gsm_create.o"
+  "/home/raoxue/Documents/cbmc_verifed_juliet_test_case_ground_truth_new/libsox-sox-14.4.2/libgsm/gsm_decode.c" "/home/raoxue/Documents/cbmc_verifed_juliet_test_case_ground_truth_new/libsox-sox-14.4.2/libgsm/CMakeFiles/gsm.dir/gsm_decode.o"
+  "/home/raoxue/Documents/cbmc_verifed_juliet_test_case_ground_truth_new/libsox-sox-14.4.2/libgsm/gsm_destroy.c" "/home/raoxue/Documents/cbmc_verifed_juliet_test_case_ground_truth_new/libsox-sox-14.4.2/libgsm/CMakeFiles/gsm.dir/gsm_destroy.o"
+  "/home/raoxue/Documents/cbmc_verifed_juliet_test_case_ground_truth_new/libsox-sox-14.4.2/libgsm/gsm_encode.c" "/home/raoxue/Documents/cbmc_verifed_juliet_test_case_ground_truth_new/libsox-sox-14.4.2/libgsm/CMakeFiles/gsm.dir/gsm_encode.o"
+  "/home/raoxue/Documents/cbmc_verifed_juliet_test_case_ground_truth_new/libsox-sox-14.4.2/libgsm/gsm_option.c" "/home/raoxue/Documents/cbmc_verifed_juliet_test_case_ground_truth_new/libsox-sox-14.4.2/libgsm/CMakeFiles/gsm.dir/gsm_option.o"
+  "/home/raoxue/Documents/cbmc_verifed_juliet_test_case_ground_truth_new/libsox-sox-14.4.2/libgsm/long_term.c" "/home/raoxue/Documents/cbmc_verifed_juliet_test_case_ground_truth_new/libsox-sox-14.4.2/libgsm/CMakeFiles/gsm.dir/long_term.o"
+  "/home/raoxue/Documents/cbmc_verifed_juliet_test_case_ground_truth_new/libsox-sox-14.4.2/libgsm/lpc.c" "/home/raoxue/Documents/cbmc_verifed_juliet_test_case_ground_truth_new/libsox-sox-14.4.2/libgsm/CMakeFiles/gsm.dir/lpc.o"
+  "/home/raoxue/Documents/cbmc_verifed_juliet_test_case_ground_truth_new/libsox-sox-14.4.2/libgsm/preprocess.c" "/home/raoxue/Documents/cbmc_verifed_juliet_test_case_ground_truth_new/libsox-sox-14.4.2/libgsm/CMakeFiles/gsm.dir/preprocess.o"
+  "/home/raoxue/Documents/cbmc_verifed_juliet_test_case_ground_truth_new/libsox-sox-14.4.2/libgsm/rpe.c" "/home/raoxue/Documents/cbmc_verifed_juliet_test_case_ground_truth_new/libsox-sox-14.4.2/libgsm/CMakeFiles/gsm.dir/rpe.o"
+  "/home/raoxue/Documents/cbmc_verifed_juliet_test_case_ground_truth_new/libsox-sox-14.4.2/libgsm/short_term.c" "/home/raoxue/Documents/cbmc_verifed_juliet_test_case_ground_truth_new/libsox-sox-14.4.2/libgsm/CMakeFiles/gsm.dir/short_term.o"
+  "/home/raoxue/Documents/cbmc_verifed_juliet_test_case_ground_truth_new/libsox-sox-14.4.2/libgsm/table.c" "/home/raoxue/Documents/cbmc_verifed_juliet_test_case_ground_truth_new/libsox-sox-14.4.2/libgsm/CMakeFiles/gsm.dir/table.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ libgsm/CMakeFiles/gsm.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gsm.dir/add.o"
+  "CMakeFiles/gsm.dir/code.o"
+  "CMakeFiles/gsm.dir/decode.o"
+  "CMakeFiles/gsm.dir/gsm_create.o"
+  "CMakeFiles/gsm.dir/gsm_decode.o"
+  "CMakeFiles/gsm.dir/gsm_destroy.o"
+  "CMakeFiles/gsm.dir/gsm_encode.o"
+  "CMakeFiles/gsm.dir/gsm_option.o"
+  "CMakeFiles/gsm.dir/long_term.o"
+  "CMakeFiles/gsm.dir/lpc.o"
+  "CMakeFiles/gsm.dir/preprocess.o"
+  "CMakeFiles/gsm.dir/rpe.o"
+  "CMakeFiles/gsm.dir/short_term.o"
+  "CMakeFiles/gsm.dir/table.o"
+  "libgsm.a"
+  "libgsm.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/gsm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
