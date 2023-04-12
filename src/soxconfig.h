c5d8dc360bffa58be8cdbb82b6853056d354f101
coverity134 <129744590+coverity134@users.noreply.github.com>
2023-04-12 19:57:07 +0800

    Add files via upload

---
+++ src/soxconfig.h
@@ -0,0 +1,58 @@
+#define PACKAGE_VERSION "14.4.2"
+
+/* #undef EXTERNAL_GSM */
+/* #undef EXTERNAL_LPC10 */
+#define HAVE_ALSA                1
+/* #undef HAVE_AMRNB */
+/* #undef HAVE_AMRWB */
+/* #undef HAVE_AO */
+#define HAVE_BYTESWAP_H          1
+/* #undef HAVE_COREAUDIO */
+#define HAVE_FENV_H              1
+/* #undef HAVE_FLAC */
+#define HAVE_FMEMOPEN            1
+#define HAVE_FSEEKO              1
+#define HAVE_GETTIMEOFDAY        1
+#define HAVE_GLOB_H              1
+#define HAVE_GSM                      1
+/* #undef HAVE_ID3TAG */
+#define HAVE_INTTYPES_H          1
+/* #undef HAVE_IO_H */
+/* #undef HAVE_LAME_LAME_H */
+/* #undef HAVE_LAME_SET_VBR_QUALITY */
+#define HAVE_LPC10                    1
+#define HAVE_LRINT               1
+/* #undef HAVE_LTDL_H */
+/* #undef HAVE_MACHINE_SOUNDCARD_H */
+/* #undef HAVE_MAD_H */
+/* #undef HAVE_MAGIC */
+#define HAVE_MKSTEMP             1
+/* #undef HAVE_MP3 */
+#define HAVE_OGG_VORBIS          1
+#define HAVE_OSS                 1
+#define HAVE_PNG                 1
+#define HAVE_POPEN               1
+#define HAVE_PULSEAUDIO          1
+/* #undef HAVE_SNDFILE */
+/* #undef HAVE_SNDFILE_1_0_18 */
+#define HAVE_SNDIO               1
+/* #undef HAVE_SPEEXDSP */
+#define HAVE_STDINT_H            1
+#define HAVE_STRCASECMP          1
+#define HAVE_STRING_H            1
+#define HAVE_STRINGS_H           1
+/* #undef HAVE_STRRSTR */
+/* #undef HAVE_SUN_AUDIO */
+/* #undef HAVE_SUN_AUDIOIO_H */
+/* #undef HAVE_SYS_AUDIOIO_H */
+#define HAVE_SYS_SOUNDCARD_H     1
+#define HAVE_SYS_STAT_H          1
+#define HAVE_SYS_TIMEB_H         1
+#define HAVE_SYS_TIME_H          1
+#define HAVE_SYS_TYPES_H         1
+#define HAVE_SYS_UTSNAME_H       1
+#define HAVE_TERMIOS_H           1
+#define HAVE_UNISTD_H            1
+#define HAVE_VSNPRINTF           1
+/* #undef HAVE_WAVPACK */
+/* #undef WORDS_BIGENDIAN */
